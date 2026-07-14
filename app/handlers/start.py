547fb48bb6ef7d14f8a36fd8f331ca81f231c689
sgushonkato-maker <sgushonkato@gmail.com>
2026-07-14 13:03:21 +0300

    Create start.py

---
+++ app/handlers/start.py
@@ -0,0 +1,33 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+from app.keyboards.main_menu import get_main_menu
+from app.config import USER_NAME, BOT_CHARACTER
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: Message):
+    text = f"""
+🌙 <b>Привет, {USER_NAME}!</b>
+
+Я <b>{BOT_CHARACTER}</b> — помощница <b>CutePix AI</b>. 💖
+
+✨ Здесь ты сможешь превращать свои фотографии в красивые ИИ-арты.
+
+🎀 Бесплатные стили:
+• Аниме
+• Сёдзё
+• Дарк Кьют
+• Ghibli
+• Фэнтези
+
+Выбери действие в меню ниже.
+"""
+
+    await message.answer(
+        text,
+        reply_markup=get_main_menu()
+    )
