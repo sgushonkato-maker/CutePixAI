100cddfd688568841fbf66c1155eb7abd8005e10
sgushonkato-maker <sgushonkato@gmail.com>
2026-07-14 13:53:22 +0300

    Create create_art.py

---
+++ app/handlers/create_art.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(lambda message: message.text == "🎨 Создать арт")
+async def create_art(message: Message):
+    await message.answer(
+        "🌙 Луннышко, отправь фотографию, которую хочешь превратить в арт! 💖"
+    )
