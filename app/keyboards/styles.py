b34d1f416a944ade510827451a8735a08df0293c
sgushonkato-maker <sgushonkato@gmail.com>
2026-07-14 14:12:44 +0300

    Update styles.py

--- app/keyboards/styles.py
+++ app/keyboards/styles.py
@@ -3,21 +3,20 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def styles_keyboard():
-
     kb = InlineKeyboardBuilder()
 
-    kb.button(text="🎀 Аниме", callback_data="anime")
-    kb.button(text="🩷 Сёдзё", callback_data="shoujo")
+    kb.button(text="🎀 Аниме", callback_data="style_anime")
+    kb.button(text="🩷 Сёдзё", callback_data="style_shoujo")
 
-    kb.button(text="🖤 Дарк Кьют", callback_data="darkcute")
-    kb.button(text="🌿 Ghibli", callback_data="ghibli")
+    kb.button(text="🖤 Дарк Кьют", callback_data="style_darkcute")
+    kb.button(text="🌿 Ghibli", callback_data="style_ghibli")
 
-    kb.button(text="✨ Фэнтези", callback_data="fantasy")
+    kb.button(text="✨ Фэнтези", callback_data="style_fantasy")
 
-    kb.button(text="🔒 📖 Манхва", callback_data="premium")
-    kb.button(text="🔒 🧛 Вампир", callback_data="premium")
-    kb.button(text="🔒 🍓 Милота 100 уровня", callback_data="premium")
+    kb.button(text="🔒 📖 Манхва", callback_data="premium_manhwa")
+    kb.button(text="🔒 🧛 Вампир", callback_data="premium_vampire")
+    kb.button(text="🔒 🍓 Милота 100 уровня", callback_data="premium_supercute")
 
-    kb.adjust(2,2,1,1,1,1)
+    kb.adjust(2, 2, 1, 1, 1, 1)
 
     return kb.as_markup()
