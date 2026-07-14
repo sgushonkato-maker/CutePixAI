c7ca706442b6968904c9f41dd3a405987f4acc7a
sgushonkato-maker <sgushonkato@gmail.com>
2026-07-14 14:02:06 +0300

    Update photo.py

--- app/handlers/photo.py
+++ app/handlers/photo.py
@@ -9,6 +9,9 @@ async def get_photo(message: Message):
 
     photo = message.photo[-1]
 
+    from app.keyboards.styles import styles_keyboard
+    
     await message.answer(
-        "🌸 Фото получено!\n\nТеперь выбери стиль 👇"
-    )
+    "🌸 Фото получено!\n\nВыбери стиль, Луннышко 💖",
+    reply_markup=styles_keyboard()
+)
