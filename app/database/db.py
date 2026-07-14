efcffdbc87e407941701f7691b0bda79691e0a45
sgushonkato-maker <sgushonkato@gmail.com>
2026-07-14 14:47:55 +0300

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -20,12 +20,12 @@ async def init_db():
 
             premium INTEGER DEFAULT 0,
             referrals INTEGER DEFAULT 0,
-            
-            invited_by INTEGER DEFAULT NULL
-            
-            last_bonus TEXT
+
+            invited_by INTEGER DEFAULT NULL,
+            last_bonus TEXT,
 
             streak INTEGER DEFAULT 1,
+
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
         """)
@@ -33,19 +33,35 @@ async def init_db():
         await db.commit()
 
 
-async def register_user(user):
+async def register_user(user, invited_by=None):
+
     async with aiosqlite.connect(DATABASE_NAME) as db:
+
+        cursor = await db.execute(
+            """
+            SELECT telegram_id
+            FROM users
+            WHERE telegram_id=?
+            """,
+            (user.id,)
+        )
+
+        exists = await cursor.fetchone()
+
+        if exists:
+            return False
 
         await db.execute(
             """
-            INSERT OR IGNORE INTO users(
+            INSERT INTO users(
                 telegram_id,
                 username,
                 first_name,
                 crystals,
-                energy
+                energy,
+                invited_by
             )
-            VALUES (?, ?, ?, ?, ?)
+            VALUES(?,?,?,?,?,?)
             """,
             (
                 user.id,
@@ -53,13 +69,33 @@ async def register_user(user):
                 user.first_name,
                 START_CRYSTALS,
                 START_ENERGY,
-            ),
+                invited_by
+            )
         )
+
+        if invited_by:
+
+            await db.execute(
+                """
+                UPDATE users
+                SET
+
+                    referrals = referrals + 1,
+
+                    crystals = crystals + 100
+
+                WHERE telegram_id=?
+                """,
+                (invited_by,)
+            )
 
         await db.commit()
 
+        return True
+
 
 async def get_profile(user_id):
+
     async with aiosqlite.connect(DATABASE_NAME) as db:
 
         cursor = await db.execute(
@@ -71,9 +107,9 @@ async def get_profile(user_id):
                 referrals,
                 streak
             FROM users
-            WHERE telegram_id = ?
+            WHERE telegram_id=?
             """,
-            (user_id,),
+            (user_id,)
         )
 
         return await cursor.fetchone()
