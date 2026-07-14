48db9ef03c52b0f22574bf59c05a9ac50c4fbf39
sgushonkato-maker <sgushonkato@gmail.com>
2026-07-14 14:10:52 +0300

    Update create_art.py

--- app/handlers/create_art.py
+++ app/handlers/create_art.py
@@ -1,12 +1,19 @@
 from aiogram import Router
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+
+from app.states.art_state import ArtState
 
 router = Router()
 
 
 @router.message(lambda message: message.text == "🎨 Создать арт")
-async def create_art(message: Message):
-    await message.answer(
-        "🌙 Луннышко, отправь фотографию, которую хочешь превратить в арт! 💖"
-    )
+async def create_art(message: Message, state: FSMContext):
+
     await state.set_state(ArtState.waiting_photo)
+
+    await message.answer(
+        "🌙 <b>Жду твою фотографию, Луннышко!</b>\n\n"
+        "📷 Отправь одно изображение.\n\n"
+        "✨ Я превращу его в настоящее маленькое чудо 💖"
+    )
